a43eba11fce4ab82a9567dd93a448bbfd5bb23bc
Emircan1122 <Aniloeker@hotmail.com>
2024-06-12 18:46:43 +0200

    I Am Mountan Guy

---
+++ app/src/main/java/com/example/mountanguy/registration.java
@@ -0,0 +1,38 @@
+package com.example.mountanguy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void launchPersonalisierung(View v) {
+
+        Intent i = new Intent(this, personalisierung.class);
+        startActivity(i);
+    }
+
+
+    public void launchAnmeldung(View v) {
+
+        Intent r = new Intent(this, MainActivity.class);
+        startActivity(r);
+    }
+}
