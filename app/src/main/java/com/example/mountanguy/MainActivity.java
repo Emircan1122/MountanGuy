7ce0defd68153fcee15bd2454d7aa8ac5a9d4bf0
Nayon0505 <nxyon.lenz@gmail.com>
2024-06-13 18:39:30 +0200

    Code von heute datenbank schließt sich immer beim ausführen

--- app/src/main/java/com/example/mountanguy/MainActivity.java
+++ app/src/main/java/com/example/mountanguy/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.mountanguy;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 private EditText namelogin , passwortlogin;
 private Button buttonlogin;
 private DataBaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +33,15 @@ private DataBaseHelper myDB;
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
-namelogin = findViewById(R.id.loginname);
-passwortlogin = findViewById(R.id.loginpasswort);
+            namelogin = findViewById(R.id.loginname);
+            passwortlogin = findViewById(R.id.loginpasswort);
+            buttonlogin = findViewById(R.id.loginbutton);
 
-buttonlogin = findViewById(R.id.loginbutton);
+            myDB = DataBaseHelper.getInstance(this);
 
-myDB = new DataBaseHelper(this);
+                myDB.openDatabase();
 
 loginUser();
 
@@ -46,12 +54,20 @@ loginUser();
                 if(var){
                     Toast.makeText(MainActivity.this, "Anmeldung Erfolgreich", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(MainActivity.this , Home.class));
+                    Log.d("Anmeldung: " ,
+                            "Name: " +namelogin.getText().toString()+
+                                 " Passwort: "+passwortlogin.getText().toString());
                     finish();
                 }else{
                     Toast.makeText(MainActivity.this, "Fehler bei der Anmeldung", Toast.LENGTH_SHORT).show();
+                    Log.d("Fehler Anmeldung: " ,
+                            "Name: " +namelogin.getText().toString()+
+                                    " Passwort: "+passwortlogin.getText().toString());
                 }
             }
         });
+
+
         }
         public void launchRegistration(View v){
 
@@ -63,5 +79,6 @@ loginUser();
         Intent h = new Intent(this, Home.class);
         startActivity(h);
     }
+
 
 }
